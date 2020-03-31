3c7f2ed205ed7a5da147ea6646c996b8f3c3aedf
tylorschafer <tylorschafer@gmail.com>
2020-03-31 13:17:28 -0600

    Switch to LinearRegression from svm, confidence boosted to 98%

--- stocks.py
+++ stocks.py
@@ -5,37 +5,25 @@ from sklearn import preprocessing, model_selection, svm
 from sklearn.linear_model import LinearRegression
 
 df = quandl.get("WIKI/GOOGL")
-
-print(df.head())
-
 df = df[['Adj. Open',  'Adj. High',  'Adj. Low',  'Adj. Close', 'Adj. Volume']]
-
 df['HL_PCT'] = (df['Adj. High'] - df['Adj. Low']) / df['Adj. Close'] * 100.0
 df['PCT_change'] = (df['Adj. Close'] - df['Adj. Open']) / df['Adj. Open'] * 100.0
 
 df = df[['Adj. Close', 'HL_PCT', 'PCT_change', 'Adj. Volume']]
-print(df.head())
-
 forecast_col = 'Adj. Close'
 df.fillna(value=-99999, inplace=True)
 forecast_out = int(math.ceil(0.01 * len(df)))
-
 df['label'] = df[forecast_col].shift(-forecast_out)
 
-df.dropna(inplace=True)
-
 X = np.array(df.drop(['label'], 1))
-y = np.array(df['label'])
-
 X = preprocessing.scale(X)
+X = X[:-forecast_out]
+df.dropna(inplace=True)
 y = np.array(df['label'])
-
 X_train, X_test, y_train, y_test = model_selection.train_test_split(X, y, test_size=0.2)
 
-clf = svm.SVR()
-
+clf = LinearRegression(n_jobs=-1)
 clf.fit(X_train, y_train)
-
 confidence = clf.score(X_test, y_test)
-
 print(confidence)
+
