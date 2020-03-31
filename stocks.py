8de139d0549a34a68e07aca2fb41efb26a00509a
tylorschafer <tylorschafer@gmail.com>
2020-03-31 13:23:04 -0600

    Add forecast_set data

--- stocks.py
+++ stocks.py
@@ -17,13 +17,19 @@ df['label'] = df[forecast_col].shift(-forecast_out)
 
 X = np.array(df.drop(['label'], 1))
 X = preprocessing.scale(X)
+X_lately = X[-forecast_out:]
 X = X[:-forecast_out]
-df.dropna(inplace=True)
-y = np.array(df['label'])
-X_train, X_test, y_train, y_test = model_selection.train_test_split(X, y, test_size=0.2)
 
+df.dropna(inplace=True)
+
+y = np.array(df['label'])
+
+X_train, X_test, y_train, y_test = model_selection.train_test_split(X, y, test_size=0.2)
 clf = LinearRegression(n_jobs=-1)
 clf.fit(X_train, y_train)
 confidence = clf.score(X_test, y_test)
 print(confidence)
+
+forecast_set = clf.predict(X_lately)
+print(forecast_set, confidence, forecast_out)
 
