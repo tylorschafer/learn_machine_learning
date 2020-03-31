98d6d782be6ce6e336026253832fec2d58956024
tylorschafer <tylorschafer@gmail.com>
2020-03-31 12:58:09 -0600

    Load data into numpy array and preprocess scale data

--- stocks.py
+++ stocks.py
@@ -4,7 +4,7 @@ import pandas as pd
 from sklearn import preprocessing, cross_validation, svm
 from sklearn.linear_model import LinearRegression
 
-df = Quandl.get("WIKI/GOOGL")
+df = quandl.get("WIKI/GOOGL")
 
 print(df.head())
 
@@ -21,3 +21,11 @@ df.fillna(value=-99999, inplace=True)
 forecast_out = int(math.ceil(0.01 * len(df)))
 
 df['label'] = df[forecast_col].shift(-forecast_out)
+
+df.dropna(inplace=True)
+
+X = np.array(df.drop(['label'], 1))
+y = np.array(df['label'])
+
+X = preprocessing.scale(X)
+y = np.array(df['label'])
